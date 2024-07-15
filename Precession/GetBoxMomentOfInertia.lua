c4d86fbb4cb8efc6fa89fc637f99adc4e59093ec
AxisAngles <AxisAngles@Gmail.com>
2024-07-15 00:07:52 -0700

    Fixed misnamed variable reference

--- Precession/GetBoxMomentOfInertia.lua
+++ Precession/GetBoxMomentOfInertia.lua
@@ -1,5 +1,5 @@
 local function getBoxMomentOfInertia(size, density)
-	local x, y, z = s.x, s.y, s.z
+	local x, y, z = size.x, size.y, size.z
 	local m = density*x*y*z
 	return CFrame.new(0, 0, 0,
 		m*(y*y + z*z)/12, 0, 0,
